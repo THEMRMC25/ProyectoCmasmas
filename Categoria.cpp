d9f57fb3f397abcd78529ece36b85b78dbf7013d
mayk <maykacopa1@gmail.com>
2021-05-06 20:53:50 -0500

    clase datos agregada

--- Categoria.cpp
+++ Categoria.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
 #include<string>
 using namespace std;
+
+class Datos
+{
+	public:
+		//setter
+		void setIsfound(bool isfound){
+			isfoundC = isfound;
+		}
+		void setNombreCategoria(string NombreCategoria){
+			NombreCategoriaC = NombreCategoria;
+		}
+		//getter
+		bool getIsfound(){
+			return isfoundC;
+			
+		}
+		string getNombreCategoria(){
+			return NombreCategoriaC;
+		}
+	private:
+		bool isfoundC;
+		string NombreCategoriaC;
+	
+};
+
 class Categoria
 {
 	public:
@@ -19,25 +44,30 @@ class Categoria
 	private:
 		bool isFound;
 };
+    
 
-class Farmacia:public Categoria
-{
+class Farmacia:public Categoria{
+	Datos data;
 	public:
 		Farmacia(string Producto){
-			isfound=Categoria::compareTo(Producto,Productos,3);
+			data.setIsfound(Categoria::compareTo(Producto,Productos,3));
+			data.setNombreCategoria("farmacia");
 		}
-		bool isfound;	
+		Datos getData(){
+			return data;
+		}	
 	private:
 		string Productos[3]={"paracetamol","medicina","vitamina"};
 };
 
 
 int main(){
-		//this is a example of product intro to the client
+	//this is a example of product intro to the client
 	string producto = "medicina";
 	
 	 Farmacia mifarm(producto);
-	 cout<<mifarm.isfound;
+	 cout<<mifarm.getData().getIsfound()<<"\n";
+	 cout<<mifarm.getData().getNombreCategoria()<<"\n";
 }
 
 	
